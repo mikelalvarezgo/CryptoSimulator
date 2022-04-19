d4ce587c6c65e553921b507025ac66e0e0f58f12
mikelalvarezgo <mikelalvarezgo@gmail.com>
2022-04-19 16:52:15 +0200

    Implemented logger with kotlinLogging

--- src/main/kotlin/com/mikelalvarezgo/crypto_wolf/Application.kt
+++ src/main/kotlin/com/mikelalvarezgo/crypto_wolf/Application.kt
@@ -2,13 +2,16 @@ package com.mikelalvarezgo.crypto_wolf
 
 import com.mikelalvarezgo.crypto_wolf.plugins.configureRouting
 import com.mikelalvarezgo.crypto_wolf.plugins.configureSerialization
+import com.mikelalvarezgo.crypto_wolf.shared.infrastructure.logger.KotlinLoggingLogger
 import io.ktor.server.application.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-    println("Starting application....")
+    val logger: KotlinLoggingLogger = KotlinLoggingLogger("crypto-wolf")
+
+    logger.info("Starting application....")
     configureRouting()
     configureSerialization()
-    println("READY!")
+    logger.info("READY!")
 }
