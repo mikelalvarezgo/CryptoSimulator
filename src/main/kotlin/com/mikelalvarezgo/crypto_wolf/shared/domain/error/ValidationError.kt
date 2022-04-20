2020f25af9973d08ad203ca2bd56bb5301f59c20
mikelalvarezgo <mikelalvarezgo@gmail.com>
2022-04-20 17:33:37 +0200

    format

--- src/main/kotlin/com/mikelalvarezgo/crypto_wolf/shared/domain/error/ValidationError.kt
+++ src/main/kotlin/com/mikelalvarezgo/crypto_wolf/shared/domain/error/ValidationError.kt
@@ -1,5 +1,5 @@
 package com.mikelalvarezgo.crypto_wolf.shared.domain.error
 
-abstract class ValidationError: Throwable() {
+abstract class ValidationError : Throwable() {
     abstract fun toMessage(): String
 }
